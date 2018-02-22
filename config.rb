b8b611e04d15878274e3de52fc42845a084c587a
Ellinor Bergman <ellinorbergman@voormedia.com>
2018-02-22 16:16:19 +0100

    Innego project.

---
+++ config.rb
@@ -0,0 +1,51 @@
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+# Set specific Template Languages
+set :haml, { :format => :html5 }
+
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# Rendering settings of Haml and Markdown.
+set :haml, attr_wrapper: '"'
+set :markdown, layout_engine: :haml
+set :markdown_engine, :redcarpet
+
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
+
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
+
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
+# Helpers
+# Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
+# helpers do
+# end
+
+# Build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+
+# configure :build do
+  # activate :minify_css
+  # activate :minify_javascript
+# end
